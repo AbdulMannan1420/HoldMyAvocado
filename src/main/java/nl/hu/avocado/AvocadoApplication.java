4f519d0a39c222ec85fd12f6b7d5d49a5059a253
Abdul Mannan <abdul.abdulmannanalam@student.hu.nl>
2024-04-16 14:28:19 +0200

    H2 database ingesteld

--- src/main/java/nl/hu/avocado/AvocadoApplication.java
+++ src/main/java/nl/hu/avocado/AvocadoApplication.java
@@ -1,13 +1,21 @@
 package nl.hu.avocado;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AvocadoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvocadoApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner run() {
+		return args -> {
+		//hier komt data te staan die je wilt toevoegen aan de database
+		};
 	}
 
 }
