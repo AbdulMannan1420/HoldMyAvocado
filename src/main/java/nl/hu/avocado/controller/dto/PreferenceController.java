1f01aa7d8903db42a09b49418161285ea336e547
Abdul Mannan <abdul.abdulmannanalam@student.hu.nl>
2024-05-16 16:52:57 +0200

    Html-form geeft nu preference die ontvang je met PreferenceController.java met PreferenceDTO.java

---
+++ src/main/java/nl/hu/avocado/controller/dto/PreferenceController.java
@@ -0,0 +1,21 @@
+package nl.hu.avocado.controller.dto;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("preference/")
+public class PreferenceController {
+
+    @PostMapping(value = "/receive", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String receiveFormData(@ModelAttribute PreferenceDTO preferenceDTO) {
+        // dit moet uiteindelijk naar opgeslagen worden in de database om preferences van de gebruiker te bewaren
+        System.out.println("Content preference: " + preferenceDTO.getContentOptions());
+        System.out.println("Day preference: " + preferenceDTO.getDayOptions());
+        System.out.println("Time preference: " + preferenceDTO.getTimeOptions());
+        return "Form data received successfully!";
+    }
+}
